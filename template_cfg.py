f2d05fb8c88ccba78e74f80d11741c5ceba6e8ce
Jan Kaspar <jan.kaspar@gmail.com>
2020-04-16 15:03:53 +0200

    Use era modifier list.

--- template_cfg.py
+++ template_cfg.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process("CTPPSTestProtonReconstruction", eras.Run2_$year)
+process = cms.Process("CTPPSTestProtonReconstruction", $era_modifiers)
 
 # minimum of logs
 process.MessageLogger = cms.Service("MessageLogger",
